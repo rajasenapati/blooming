40a4beca0523f2eec34fce1d68cc6e377256d990
Raja Senapati <raja.ranjan@gmail.com>
2021-01-31 20:38:55 -0800

    demo of selective post processing with a dot and bloom effects

---
+++ js/selectivePassDemo1.js
@@ -0,0 +1,100 @@
+var scene, camera, renderer, cube, cube2,  ambient, composer;
+
+function setupPostProcessing() {
+  const renderPass = new THREE.RenderPass(scene, camera);
+  composer.addPass(renderPass);
+
+  //add a DotScreenPass pass. this adds a very cool monochromatic effect of dots replacing a surface.
+  //change the scale from 0.3 to 1 to 3 to get cool patterns
+  // let center = new THREE.Vector2( 0.1, 0.9 ),
+  // let angle = Math.PI/4,
+  // let scale = 0.3
+  const pass = new THREE.DotScreenPass(undefined, undefined, 0.3);
+  composer.addPass(pass);
+  pass.renderToScreen = true;
+
+}
+
+
+function createBasicGeometryAndLights() {
+  const geometry = new THREE.BoxGeometry();
+  let material = new THREE.MeshStandardMaterial({
+    side: THREE.DoubleSide,
+    color: 0x7fc5f9,
+    emissive: 0x25673d,
+    emissiveIntensity: 0.4,
+    metalness: 0.5, //0 for wood, 1 for metal. Default0.5
+    roughness: 0.3 // 0 for smooth mirror reflection, 1 for fully defuse
+  })
+
+  let material2 = new THREE.MeshStandardMaterial({
+    side: THREE.DoubleSide,
+    color: 0xff0000,
+    emissive: 0x25673d,
+    emissiveIntensity: 0.4,
+    metalness: 0.5, //0 for wood, 1 for metal. Default0.5
+    roughness: 0.3 // 0 for smooth mirror reflection, 1 for fully defuse
+  })
+
+  cube = new THREE.Mesh(geometry, material);
+  cube2 = new THREE.Mesh(geometry, material2);
+  cube2.position.set(0.5, 0.5, -2);
+  var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  scene.add( directionalLight );
+  directionalLight.target = cube;
+
+  cube.name = 'cube';
+  cube2.name = 'cube2';
+  scene.add( cube );
+  scene.add(cube2);
+  ambient = new THREE.AmbientLight(0xffffff, 0.3);  //color and intensity
+  scene.add( ambient);
+}
+
+var init = function() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  renderer = new THREE.WebGLRenderer();
+  composer = new THREE.EffectComposer(renderer);
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  composer.setSize( window.innerWidth, window.innerHeight );
+
+  //setup post processing
+  setupPostProcessing();
+
+  document.body.appendChild( renderer.domElement );
+  window.addEventListener( 'resize', onWindowResize, false);
+  createBasicGeometryAndLights();
+
+  camera.position.z = 5;
+
+  const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.target.set(0,0,0);
+  controls.update();
+
+}
+
+var animate = function () {
+  requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube2.rotation.x -= 0.01;
+  cube2.rotation.y -= 0.01;
+
+  // renderer.render( scene, camera );
+  composer.render();
+};
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  composer.setSize( window.innerWidth, window.innerHeight );
+  console.log(renderer.domElement.width, renderer.domElement.height);
+}
+
+
+init();
+animate();
+
